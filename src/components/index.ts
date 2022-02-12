60583342fefee767a0d71d4540a7f7283eed3619
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-02-12 18:06:17 +0200

    Remove `Heading`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Copyright } from './Copyright';
 export { default as TopBar } from './TopBar';
 export { default as Layout } from './Layout';
-export { default as Heading } from './Heading';
