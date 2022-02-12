c7fd6beaf032ca0c27853e1ac87f1fa00808e428
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-02-12 12:31:03 +0200

    Connect `responsiveFontSizes` to the theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
 // A custom theme for this app
@@ -16,4 +16,4 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
