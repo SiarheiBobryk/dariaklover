5b8f754bc9be20452921450af84f1a1a0ac6ca48
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-03-21 22:59:57 +0200

    Add the lazy loading feature for the pages #73

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,46 @@ import { Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 import { Theme, useTheme } from '@mui/material/styles';
+import LinearProgress from '@mui/material/LinearProgress';
 
 import Layout from './components/Layout';
-import { Main, About, Archive } from './pages';
+
 import { AppConfigContext, Config } from './providers/AppConfigProvider';
+
+const About = React.lazy(function lazyLoadAbout() {
+  return import('./pages/About');
+});
+
+const Archive = React.lazy(function lazyLoadArchive() {
+  return import('./pages/Archive');
+});
+
+// TODO: Uncomment it when I've done testing
+const Main = React.lazy(function lazyLoadMain() {
+  return import('./pages/Main');
+});
+
+// TODO: Remove it after testing
+// const Main = React.lazy(() => {
+//   return import('./pages/Main').then((module) => {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+//         resolve(module as any);
+//       }, 2000);
+//     });
+//   });
+// });
+
+interface SuspenseProps {
+  children: React.ReactNode;
+}
+
+function Suspense(props: SuspenseProps) {
+  const { children } = props;
+  // TODO: Add the appropriate fallback component here
+  return <React.Suspense fallback={<LinearProgress />}>{children}</React.Suspense>;
+}
 
 function App() {
   const theme: Theme = useTheme();
@@ -20,14 +56,15 @@ function App() {
         <meta name="keywords" content={keywords} />
         <meta name="theme-color" content={theme.palette.background.default} />
       </Helmet>
-      <Layout>
-        <Routes>
-          {/* TODO: Use lazy modules here */}
-          <Route path="/about" element={<About />} />
-          <Route path="/archive" element={<Archive />} />
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </Layout>
+      <Suspense>
+        <Layout>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Layout>
+      </Suspense>
     </>
   );
 }
