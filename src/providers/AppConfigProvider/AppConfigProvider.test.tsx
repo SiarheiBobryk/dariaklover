e1c3b283c223b2233a78c1ee1ea6b56ec3642b5c
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-04-26 21:23:50 +0300

    Add tests for `AppConfigProvider` (#135)

---
+++ src/providers/AppConfigProvider/AppConfigProvider.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+
+import { AppConfigContext, Config, config } from './AppConfigProvider';
+
+describe('AppConfigProvider', () => {
+  describe('The context object inspection', () => {
+    it('should hold all the values of the `config` object', () => {
+      let context;
+
+      render(
+        <AppConfigContext.Consumer>
+          {function appConfigContext(value: Config) {
+            context = value;
+            return <div />;
+          }}
+        </AppConfigContext.Consumer>,
+      );
+
+      expect(context).toEqual(config);
+    });
+  });
+});
