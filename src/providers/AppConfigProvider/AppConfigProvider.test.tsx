3bb621953a53c62ceb5da72b938d5dfea95a0f8a
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-05-01 11:40:25 +0300

    Add types for a `AppConfigProvider`'s test case (#165)

--- src/providers/AppConfigProvider/AppConfigProvider.test.tsx
+++ src/providers/AppConfigProvider/AppConfigProvider.test.tsx
@@ -7,7 +7,7 @@ import { AppConfigContext, Config, config } from './AppConfigProvider';
 describe('AppConfigProvider', () => {
   describe('The context object inspection', () => {
     it('should hold all the values of the `config` object', () => {
-      let context;
+      let context: Config | undefined;
 
       render(
         <AppConfigContext.Consumer>
