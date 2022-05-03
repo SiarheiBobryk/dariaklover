6294b0f5499470210ec66cf906703c7183a40f52
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-05-03 16:55:53 +0300

    ForwardRefs for the `Heading` component

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -11,12 +11,18 @@ import Heading from './Heading';
 expect.extend(toHaveNoViolations);
 
 describe('<Heading />', () => {
-  describe('a11y', () => {
+  describe('conformance', () => {
     it('should match a11y standards', async () => {
       const { container }: RenderResult = render(<Heading>Lorem Ipsulum</Heading>);
 
       const results = await axe(container);
       expect(results).toHaveNoViolations();
+    });
+
+    it('should forward ref', () => {
+      const ref = React.createRef();
+      const { container } = render(<Heading ref={ref} />);
+      expect(container.firstChild).toBe(ref.current);
     });
   });
 
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -23,14 +23,16 @@ export interface HeadingProps {
     | undefined;
 }
 
-function Heading(props: HeadingProps) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Heading = React.forwardRef(function Heading(props: HeadingProps, ref: React.Ref<any>) {
+  // TODO: Move default props to the global config
   const { children, component = 'h2', variant = 'h5', ...other } = props;
 
   return (
-    <Typography component={component} variant={variant} {...other}>
+    <Typography ref={ref} component={component} variant={variant} {...other}>
       {children}
     </Typography>
   );
-}
+});
 
 export default Heading;
