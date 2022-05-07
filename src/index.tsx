abda52ed21f132431c55baf4bf4f8f295e85266e
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-05-07 17:56:16 +0300

    Set `React.StrictMode` for the dev env only

--- src/index.tsx
+++ src/index.tsx
@@ -11,18 +11,20 @@ import App from './App';
 import '@fontsource/roboto/400.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <AppConfigProvider>
-      <ColorModeProvider>
-        <ThemeProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ThemeProvider>
-      </ColorModeProvider>
-    </AppConfigProvider>
-  </React.StrictMode>,
+
+const ui = (
+  <AppConfigProvider>
+    <ColorModeProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ColorModeProvider>
+  </AppConfigProvider>
 );
+const app = process.env.NODE_ENV === 'development' ? <React.StrictMode>{ui}</React.StrictMode> : ui;
+
+root.render(app);
 
 // TODO: Add web vitals
