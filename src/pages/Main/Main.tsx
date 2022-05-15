7d2749f9e6158bfb2606d4edd14539cc79c19794
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-05-15 10:24:14 +0300

    Add the metadata feature to `Main`\`Introduction`

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,14 +3,23 @@ import { Helmet } from 'react-helmet';
 
 import Introduction from '../../posts/Introduction/Introduction';
 
+export interface MainMetaData {
+  title: string;
+  heading: string;
+}
+
+export const mainMetaData: MainMetaData = {
+  title: 'Главная страница',
+  heading: 'Кто я и что я делаю',
+};
+
 function Main() {
   return (
     <>
       <Helmet>
-        <title>Главная страница</title>
+        <title>{mainMetaData.title}</title>
       </Helmet>
-
-      <Introduction />
+      <Introduction heading={mainMetaData.heading} />
     </>
   );
 }
