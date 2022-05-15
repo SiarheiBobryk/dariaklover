773e1ca721919f0ca6ff13a4aa601ab17b5094e0
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-05-15 12:48:12 +0300

    Add tests for the Booking page

---
+++ src/pages/Booking/Booking.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Helmet, HelmetPropsToState } from 'react-helmet';
+
+import { render, RenderResult } from '@testing-library/react';
+import renderer, { ReactTestRendererJSON } from 'react-test-renderer';
+import { axe, toHaveNoViolations } from 'jest-axe';
+
+import Booking from './Booking';
+import bookingMetaData from './bookingMetaData';
+
+expect.extend(toHaveNoViolations);
+
+describe('<Booking />', () => {
+  describe('a11y', () => {
+    it('should match a11y standards', async () => {
+      const { container }: RenderResult = render(<Booking />);
+
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    });
+  });
+
+  describe('snapshotting', () => {
+    it('should render correctly', () => {
+      const about: ReactTestRendererJSON | ReactTestRendererJSON[] | null = renderer.create(<Booking />).toJSON();
+      expect(about).toMatchSnapshot();
+    });
+  });
+
+  it('should have the right title HTML element', () => {
+    render(<Booking />);
+
+    const { title }: HelmetPropsToState = Helmet.peek();
+    expect(title).toBe(bookingMetaData.title);
+  });
+});
