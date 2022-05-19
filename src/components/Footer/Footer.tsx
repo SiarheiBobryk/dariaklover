4d473e5a6d7bd78af98fe509457a83d5dfbb1719
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-05-19 18:10:12 +0300

    Add `Tooltip`s to non-text buttons (#251)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,6 +8,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import EmailIcon from '@mui/icons-material/Email';
+import Tooltip from '@mui/material/Tooltip';
 
 import { AppConfigContext, Config } from '../../providers/AppConfigProvider';
 import packageJson from '../../../package.json';
@@ -35,30 +36,40 @@ function Footer(props: FooterProps) {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 1, marginBottom: 1 }} {...props}>
       <Box>
-        <IconButton aria-label={instagramLabel} href={instagramHref} color="inherit" {...iconButtonA11yProps}>
-          <InstagramIcon />
-        </IconButton>
-        <IconButton aria-label={telegramLabel} href={telegramHref} color="inherit" {...iconButtonA11yProps}>
-          <TelegramIcon />
-        </IconButton>
-        <IconButton
-          aria-label="Электронный адрес Дарьи"
-          href={`mailto:  ${email}`}
-          color="inherit"
-          {...iconButtonA11yProps}
-        >
-          <EmailIcon />
-        </IconButton>
-        <IconButton aria-label={calendlyLabel} href={calendlyHref} color="inherit" {...iconButtonA11yProps}>
-          <CalendarMonthIcon />
-        </IconButton>
+        <Tooltip title="Инстаграм">
+          <IconButton aria-label={instagramLabel} href={instagramHref} color="inherit" {...iconButtonA11yProps}>
+            <InstagramIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Телеграм">
+          <IconButton aria-label={telegramLabel} href={telegramHref} color="inherit" {...iconButtonA11yProps}>
+            <TelegramIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Электронная почта">
+          <IconButton
+            aria-label="Электронный адрес Дарьи"
+            href={`mailto:  ${email}`}
+            color="inherit"
+            {...iconButtonA11yProps}
+          >
+            <EmailIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Календарь">
+          <IconButton aria-label={calendlyLabel} href={calendlyHref} color="inherit" {...iconButtonA11yProps}>
+            <CalendarMonthIcon />
+          </IconButton>
+        </Tooltip>
       </Box>
       <Typography variant="body2" color="text.secondary">
         {fullName} {new Date().getFullYear()}
       </Typography>
-      <Link variant="body2" color="text.secondary" href={`${github}/releases/tag/v${packageJson.version}`}>
-        {`v${packageJson.version}`}
-      </Link>
+      <Tooltip title="Техническая информация о последнем обновлении">
+        <Link variant="body2" color="text.secondary" href={`${github}/releases/tag/v${packageJson.version}`}>
+          {`v${packageJson.version}`}
+        </Link>
+      </Tooltip>
     </Box>
   );
 }
