d5748174869bf02a0ceabed3a93b4ef3314f1609
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2022-06-05 15:15:54 +0200

    Block the conformation dialog functionality

--- src/pages/Booking/Booking.tsx
+++ src/pages/Booking/Booking.tsx
@@ -1,19 +1,21 @@
+// TODO: Uncomment and refactor the conformation Dialog functionality once we got a payed Calendly account
+
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
-import { useSearchParams } from 'react-router-dom';
+// import { useSearchParams } from 'react-router-dom';
 
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Link from '@mui/material/Link';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
+// import Button from '@mui/material/Button';
+// import Dialog from '@mui/material/Dialog';
+// import DialogActions from '@mui/material/DialogActions';
+// import DialogContent from '@mui/material/DialogContent';
+// import DialogTitle from '@mui/material/DialogTitle';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+// import { useTheme } from '@mui/material/styles';
 
 import Heading from '../../components/Heading';
 import Paragraph from '../../components/Paragraph';
@@ -39,49 +41,50 @@ export interface CalendlyResponse {
 
 function References() {
   const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     calendly: { href: calendlyHref, appointments, conformation },
   }: Config = React.useContext(AppConfigContext);
-  const [open, setOpen] = React.useState(false);
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+  // const [open, setOpen] = React.useState(false);
+  // const theme = useTheme();
+  // const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-  const handleClose = React.useCallback(function handleClose() {
-    setOpen(false);
-  }, []);
+  // const handleClose = React.useCallback(function handleClose() {
+  //   setOpen(false);
+  // }, []);
 
-  const handleBackdropClick = React.useCallback(
-    function handleBackdropClick(_event: Event, reason: string) {
-      // Force the user to click on the action button to close the dialog
-      if (reason !== 'backdropClick') {
-        handleClose();
-      }
-    },
-    [handleClose],
-  );
+  // const handleBackdropClick = React.useCallback(
+  //   function handleBackdropClick(_event: Event, reason: string) {
+  //     // Force the user to click on the action button to close the dialog
+  //     if (reason !== 'backdropClick') {
+  //       handleClose();
+  //     }
+  //   },
+  //   [handleClose],
+  // );
 
-  const [searchParams] = useSearchParams();
+  // const [searchParams] = useSearchParams();
 
-  const calendlyResponse: CalendlyResponse = {
-    assignedTo: searchParams.get('assigned_to'),
-    eventTypeUuid: searchParams.get('event_type_uuid'),
-    eventTypeName: searchParams.get('event_type_name'),
-    eventStartTime: new Date(searchParams.get('event_start_time') ?? ''),
-    eventEndTime: new Date(searchParams.get('event_end_time') ?? ''),
-    inviteeUuid: searchParams.get('invitee_uuid'),
-    inviteeFullName: searchParams.get('invitee_full_name'),
-    inviteeEmail: searchParams.get('invitee_email'),
-    textReminderNumber: searchParams.get('text_reminder_number'),
-  };
+  // const calendlyResponse: CalendlyResponse = {
+  //   assignedTo: searchParams.get('assigned_to'),
+  //   eventTypeUuid: searchParams.get('event_type_uuid'),
+  //   eventTypeName: searchParams.get('event_type_name'),
+  //   eventStartTime: new Date(searchParams.get('event_start_time') ?? ''),
+  //   eventEndTime: new Date(searchParams.get('event_end_time') ?? ''),
+  //   inviteeUuid: searchParams.get('invitee_uuid'),
+  //   inviteeFullName: searchParams.get('invitee_full_name'),
+  //   inviteeEmail: searchParams.get('invitee_email'),
+  //   textReminderNumber: searchParams.get('text_reminder_number'),
+  // };
 
-  const { inviteeEmail } = calendlyResponse;
+  // const { inviteeEmail } = calendlyResponse;
 
-  React.useEffect(
-    function handleModalVisibility() {
-      // NOTE: `inviteeEmail` is required field for booking an appointment
-      setOpen(Boolean(inviteeEmail));
-    },
-    [inviteeEmail],
-  );
+  // React.useEffect(
+  //   function handleModalVisibility() {
+  //     // NOTE: `inviteeEmail` is required field for booking an appointment
+  //     setOpen(Boolean(inviteeEmail));
+  //   },
+  //   [inviteeEmail],
+  // );
 
   return (
     <>
@@ -104,7 +107,7 @@ function References() {
           );
         })}
       </List>
-      {conformation && (
+      {/* {conformation && (
         <Dialog
           fullScreen={fullScreen}
           open={open}
@@ -139,7 +142,7 @@ function References() {
             </Button>
           </DialogActions>
         </Dialog>
-      )}
+      )} */}
       <Alert severity="warning">
         <AlertTitle>Система тестируется</AlertTitle>
         Обращаем ваше внимание, что система записи на консультацию находится в режиме тестирования и может быть
