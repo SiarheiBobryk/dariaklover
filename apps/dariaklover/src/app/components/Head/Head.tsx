6ce128b7e024854d4ad109f8e97f5ea2a678e908
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-02-25 16:29:45 +0100

    fix: the path to icons

--- apps/dariaklover/src/app/components/Head/Head.tsx
+++ apps/dariaklover/src/app/components/Head/Head.tsx
@@ -18,14 +18,14 @@ function Head() {
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta name="theme-color" content={theme.palette.background.default} />
-      <link rel="apple-touch-icon" sizes="180x180" href={`/icon/apple-touch-icon.png?v=${packageJson.version}`} />
-      <link rel="icon" type="image/png" sizes="32x32" href={`/icon/favicon-32x32.png?v=${packageJson.version}`} />
-      <link rel="icon" type="image/png" sizes="16x16" href={`/icon/favicon-16x16.png?v=${packageJson.version}`} />
-      <link rel="manifest" href={`/icon/site.webmanifest?v=${packageJson.version}`} />
-      <link rel="mask-icon" href={`/icon/safari-pinned-tab.svg?v=${packageJson.version}`} color="#5bbad5" />
-      <link rel="icon" href={`/icon/favicon.ico?v=${packageJson.version}`} type="image/x-icon" />
+      <link rel="apple-touch-icon" sizes="180x180" href={`assets/icon/apple-touch-icon.png?v=${packageJson.version}`} />
+      <link rel="icon" type="image/png" sizes="32x32" href={`assets/icon/favicon-32x32.png?v=${packageJson.version}`} />
+      <link rel="icon" type="image/png" sizes="16x16" href={`assets/icon/favicon-16x16.png?v=${packageJson.version}`} />
+      <link rel="manifest" href={`assets/icon/site.webmanifest?v=${packageJson.version}`} />
+      <link rel="mask-icon" href={`assets/icon/safari-pinned-tab.svg?v=${packageJson.version}`} color="#5bbad5" />
+      <link rel="icon" href={`assets/icon/favicon.ico?v=${packageJson.version}`} type="image/x-icon" />
       <meta name="msapplication-TileColor" content={configs['msapplication-TileColor']} />
-      <meta name="msapplication-config" content={`/icon/browserconfig.xml?v=${packageJson.version}`} />
+      <meta name="msapplication-config" content={`assets/icon/browserconfig.xml?v=${packageJson.version}`} />
     </Helmet>
   );
 }
