b1e59a539febbfdbb31baa60c787324a13119690
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-02-25 17:25:08 +0100

    fix: "yarn test" console errors (#777)

--- apps/dariaklover/setupTests.ts
+++ apps/dariaklover/setupTests.ts
@@ -1,3 +1,6 @@
+// Polyfill "window.fetch" used in the React component.
+import 'whatwg-fetch';
+
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import '@testing-library/jest-dom';
