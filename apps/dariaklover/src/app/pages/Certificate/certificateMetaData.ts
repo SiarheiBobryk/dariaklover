6eb9097cf431ff0d7c98ae34a7bd9c8795badefa
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-03-18 17:45:08 +0100

    refactor: "certificateMetaData"

--- apps/dariaklover/src/app/pages/Certificate/certificateMetaData.ts
+++ apps/dariaklover/src/app/pages/Certificate/certificateMetaData.ts
@@ -1,29 +1,13 @@
-import { certificates } from '../../components/Certificates';
-
 export interface CertificateMetaData {
   title: string;
   heading: string;
   path: string;
-  certificateHealthCoachUrl: string;
-  certificateHealthCoachTitle: string;
-  diplomaHealthCoachUrl: string;
-  diplomaHealthCoachTitle: string;
-  diplomaDoctorUrl: string;
-  diplomaDoctorTitle: string;
 }
-
-const [certificateHealthCoach, diplomaHealthCoach, diplomaDoctor] = certificates;
 
 const certificateMetaData: CertificateMetaData = {
   title: 'Сертификат',
   heading: 'Сертификат',
   path: ':certificateId',
-  certificateHealthCoachUrl: certificateHealthCoach.url,
-  certificateHealthCoachTitle: certificateHealthCoach.title,
-  diplomaHealthCoachUrl: diplomaHealthCoach.url,
-  diplomaHealthCoachTitle: diplomaHealthCoach.title,
-  diplomaDoctorUrl: diplomaDoctor.url,
-  diplomaDoctorTitle: diplomaDoctor.title,
 };
 
 export default certificateMetaData;
