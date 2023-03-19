ebacbffc7c39d17ac80939a4ba46b4fe3a38932b
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-03-19 11:26:58 +0100

    feat: avatar on the main page

--- apps/dariaklover/src/app/pages/Main/Main.tsx
+++ apps/dariaklover/src/app/pages/Main/Main.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet';
 
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
+
 import Heading from '../../components/Heading';
 import mainMetaData from './mainMetaData';
 import Paragraph from '../../components/Paragraph';
+
+export const PUBLIC_PATH = 'assets/responsive_images';
+
+export const AvatarContainer = styled('div')(function styleAvatarContainer() {
+  return {
+    display: 'flex',
+    justifyContent: 'center',
+  };
+});
+
+export const AvatarStyled = styled(Avatar)(function styleAvatar({ theme }) {
+  return {
+    width: theme.spacing(35),
+    height: theme.spacing(35),
+    margin: theme.spacing(1),
+  };
+});
 
 function Main() {
   return (
@@ -12,6 +32,9 @@ function Main() {
         <title>{mainMetaData.title}</title>
       </Helmet>
       <Heading>{mainMetaData.heading}</Heading>
+      <AvatarContainer>
+        <AvatarStyled alt="Дарья Кловер" src={`${PUBLIC_PATH}/photo-s.jpg`} />
+      </AvatarContainer>
       <Paragraph>Всем привет!</Paragraph>
       <Paragraph>
         Я Дарья Кловер - дипломированный health-коуч, специалист по здоровому образу жизни и немедикаментозному
