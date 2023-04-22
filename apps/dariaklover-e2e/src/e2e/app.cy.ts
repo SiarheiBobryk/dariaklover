7366f525fad88ebfc4f829f97232e9955c62b639
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-04-22 20:23:37 +0200

    feat: the cypress e2e testing environment

--- apps/dariaklover-e2e/src/e2e/app.cy.ts
+++ apps/dariaklover-e2e/src/e2e/app.cy.ts
@@ -7,9 +7,9 @@ describe('dariaklover', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome dariaklover');
+    getGreeting().contains('Кто я и что делаю');
   });
 });
