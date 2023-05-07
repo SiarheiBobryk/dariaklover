558d27cc47c4f2b180fab8534032b61f52e10d83
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-05-07 15:57:37 +0200

    fix: missed heading for the pricing page

--- apps/dariaklover-e2e/src/e2e/app.cy.ts
+++ apps/dariaklover-e2e/src/e2e/app.cy.ts
@@ -9,6 +9,12 @@ describe('the site pages availability', () => {
     cy.get('.MuiBreadcrumbs-li').should('have.length', 1);
   });
 
+  it('should display the pricing page', () => {
+    cy.get('a[aria-label="Услуги"]:not([role="menuitem"])').click();
+    cy.get('h1').contains('Мои услуги и стоимость');
+    cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
+  });
+
   it('should display the references page', () => {
     cy.get('a[aria-label="Отзывы"]:not([role="menuitem"])').click();
     cy.get('h1').contains('Отзывы клиентов');
