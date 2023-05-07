4059002edaea2f6818b7d2414f9f3ccb9b57ed45
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-05-07 16:15:41 +0200

    test: add tests for the rest of main pages

--- apps/dariaklover-e2e/src/e2e/app.cy.ts
+++ apps/dariaklover-e2e/src/e2e/app.cy.ts
@@ -4,15 +4,36 @@ describe('the site pages availability', () => {
     return cy.visit('/');
   });
 
-  it('should display home page', () => {
+  it('should display the home page', () => {
     cy.get('h1').contains('Кто я и что делаю');
     cy.get('img[alt="Дарья Кловер"]').should('have.class', 'MuiAvatar-img');
     cy.get('.MuiBreadcrumbs-li').should('have.length', 1);
   });
 
+  it('should display the booking page', () => {
+    cy.intercept('GET', '/event_types?user=*', {
+      fixture: 'eventTypes.json',
+    }).as('getEventTypes');
+    cy.get('a[aria-label="Записаться"]:not([role="menuitem"])').click();
+    cy.get('h1').contains('Записаться');
+    cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
+  });
+
   it('should display the pricing page', () => {
     cy.get('a[aria-label="Услуги"]:not([role="menuitem"])').click();
     cy.get('h1').contains('Мои услуги и стоимость');
+    cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
+  });
+
+  it('should display the pricing page', () => {
+    cy.get('a[aria-label="Услуги"]:not([role="menuitem"])').click();
+    cy.get('h1').contains('Мои услуги и стоимость');
+    cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
+  });
+
+  it('should display the about page', () => {
+    cy.get('a[aria-label="Обо мне"]:not([role="menuitem"])').click();
+    cy.get('h1').contains('Обо мне');
     cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
   });
 
@@ -22,12 +43,9 @@ describe('the site pages availability', () => {
     cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
   });
 
-  it('should display the booking page', () => {
-    cy.intercept('GET', '/event_types?user=*', {
-      fixture: 'eventTypes.json',
-    }).as('getEventTypes');
-    cy.visit('http://localhost:4200/booking');
-    cy.get('h1').contains('Записаться');
+  it('should display the help page', () => {
+    cy.get('a[aria-label="Помощь"]:not([role="menuitem"])').click();
+    cy.get('h1').contains('Помощь');
     cy.get('.MuiBreadcrumbs-li').should('have.length', 2);
   });
 });
