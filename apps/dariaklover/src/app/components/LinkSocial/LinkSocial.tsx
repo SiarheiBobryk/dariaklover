b6120248943b445c325a579ed0799ef777ac1952
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-06-18 23:57:24 +0200

    refactor: the "News" component (#1337)

--- apps/dariaklover/src/app/components/LinkSocial/LinkSocial.tsx
+++ apps/dariaklover/src/app/components/LinkSocial/LinkSocial.tsx
@@ -9,8 +9,7 @@ export interface LinkSocialProps {
   social?: 'telegram' | 'instagram' | 'whatsapp';
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const LinkSocial = React.forwardRef(function LinkSocial(props: LinkSocialProps, ref: React.Ref<any>) {
+const LinkSocial = React.forwardRef(function LinkSocial(props: LinkSocialProps, ref: React.Ref<HTMLAnchorElement>) {
   const { social = 'telegram', ...other } = props;
   const config: Config = React.useContext(AppConfigContext);
 
