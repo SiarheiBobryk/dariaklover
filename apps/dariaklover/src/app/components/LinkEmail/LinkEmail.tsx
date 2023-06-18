23ac36b49ce8eb6424ba9eb747fd912b54a7a956
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-06-18 23:42:11 +0200

    refactor: the link email component

--- apps/dariaklover/src/app/components/LinkEmail/LinkEmail.tsx
+++ apps/dariaklover/src/app/components/LinkEmail/LinkEmail.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 
 import { Config } from '@dariaklover/types';
+import { LinkProps } from '@mui/material/Link';
 
 import { AppConfigContext } from '../../providers/AppConfigProvider';
 import LinkBlank from '../LinkBlank';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface LinkEmailProps {
-  // TODO: Inherit the Link component props here
-}
+export interface LinkEmailProps extends LinkProps {}
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const LinkEmail = React.forwardRef(function LinkEmail(props: LinkEmailProps, ref: React.Ref<any>) {
+const LinkEmail = React.forwardRef(function LinkEmail(props: LinkEmailProps, ref: React.Ref<HTMLAnchorElement>) {
   const {
     email: { id, labelShort },
   }: Config = React.useContext(AppConfigContext);
