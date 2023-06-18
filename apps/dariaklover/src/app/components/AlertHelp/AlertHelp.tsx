104dc527717b4a5f0dd71f5e24a7ed5e3b3d45e6
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-06-18 18:43:01 +0200

    refactor: the alert help component (#1330)

--- apps/dariaklover/src/app/components/AlertHelp/AlertHelp.tsx
+++ apps/dariaklover/src/app/components/AlertHelp/AlertHelp.tsx
@@ -7,22 +7,32 @@ import LinkEmail from '../LinkEmail';
 import LinkSocial from '../LinkSocial';
 
 export interface AlertHelpProps {
+  title?: string;
   children?: React.ReactNode;
   open?: boolean;
   onClose?: React.MouseEventHandler<HTMLButtonElement>;
   severity?: 'success' | 'info' | 'warning' | 'error';
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const AlertHelp = React.forwardRef(function AlertHelp(props: AlertHelpProps, ref: React.Ref<any>) {
-  const { children, open, onClose, ...other } = props;
+const AlertHelp = React.forwardRef(function AlertHelp(props: AlertHelpProps, ref: React.Ref<HTMLDivElement>) {
+  const {
+    title = 'Связаться с нами',
+    children = (
+      <>
+        Если у вас остались вопросы или вы продолжаете испытывать сложности в работе с сайтом, то не стесняйтесь и
+        свяжитесь с нами через <LinkSocial social="telegram" />, <LinkSocial social="whatsapp" /> или по <LinkEmail />.
+        Мы обязательно поможем.
+      </>
+    ),
+    open,
+    onClose,
+    ...other
+  } = props;
 
   return (
     <Alert ref={ref} severity="info" variant="outlined" {...other}>
-      <AlertTitle>Связаться с нами</AlertTitle>
-      Если у вас остались вопросы или вы продолжаете испытывать сложности в работе с сайтом, то не стесняйтесь и
-      свяжитесь с нами через <LinkSocial social="telegram" />, <LinkSocial social="whatsapp" /> или по <LinkEmail />. Мы
-      обязательно поможем.
+      <AlertTitle>{title}</AlertTitle>
+      {children}
     </Alert>
   );
 });
