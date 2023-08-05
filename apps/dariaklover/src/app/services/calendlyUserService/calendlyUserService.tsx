549cb0383c86880ecaea1e31e40f6d97dd1d8792
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-08-05 11:35:34 +0200

    refactor: optimize the fetch effects (#1519)

--- apps/dariaklover/src/app/services/calendlyUserService/calendlyUserService.tsx
+++ apps/dariaklover/src/app/services/calendlyUserService/calendlyUserService.tsx
@@ -20,27 +20,35 @@ const HEADERS = {
   authorization: `Bearer ${process.env.NX_CALENDLY_ACCESS_TOKEN ?? ''}`,
 } as const;
 
-export async function getCalendlyUser(): Promise<CalendlyUserDto> {
+export async function getCalendlyUser(signal: AbortSignal): Promise<CalendlyUserDto> {
   const response: Response = await fetch(CALENDLY_API.GET_CURRENT_USER, {
     headers: HEADERS,
+    signal,
   });
   const calendlyUserResponse = (await response.json()) as CalendlyUserResponse;
   const { resource: calendlyUser } = calendlyUserResponse;
   return calendlyUserToDto(calendlyUser);
 }
 
-export async function getCalendlyEvents(calendlyUserUri: string): Promise<Array<CalendlyUserEventDto>> {
+export async function getCalendlyEvents(
+  calendlyUserUri: string,
+  signal: AbortSignal,
+): Promise<Array<CalendlyUserEventDto>> {
   const LIST_USERS_EVENT_TYPES = `${CALENDLY_API.LIST_USERS_EVENT_TYPES}${calendlyUserUri}`;
   const response = await fetch(LIST_USERS_EVENT_TYPES, {
     headers: HEADERS,
+    signal,
   });
   const calendlyUserResponse = (await response.json()) as CalendlyUserEventTypeResponse;
   const { collection: calendlyEvents = [] } = calendlyUserResponse;
   return calendlyEvents.map(calendlyUserEventTypeToDto);
 }
 
-export async function getCalendlyEventsActive(calendlyUserUri: string): Promise<Array<CalendlyUserEventDto>> {
-  const calendlyEvents = await getCalendlyEvents(calendlyUserUri);
+export async function getCalendlyEventsActive(
+  calendlyUserUri: string,
+  signal: AbortSignal,
+): Promise<Array<CalendlyUserEventDto>> {
+  const calendlyEvents = await getCalendlyEvents(calendlyUserUri, signal);
   const calendlyEventsActive = calendlyEvents.filter(function isActive(event) {
     const { active } = event;
     return active;
