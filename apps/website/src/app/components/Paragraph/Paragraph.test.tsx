2da7887dadfc479deef3b3b2c60d1a89ac1b91d4
Siarhei Bobryk <siarhei.bobryk@icloud.com>
2023-10-23 12:07:52 +0200

    test: remove redundant tests (#1742)

--- apps/website/src/app/components/Paragraph/Paragraph.test.tsx
+++ apps/website/src/app/components/Paragraph/Paragraph.test.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
-import { typographyClasses } from '@mui/material/Typography';
-import { render, RenderResult, screen } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
 import Paragraph from './Paragraph';
@@ -21,45 +20,6 @@ describe('<Paragraph />', () => {
       const ref = React.createRef<HTMLParagraphElement>();
       const { container } = render(<Paragraph ref={ref} />);
       expect(container.firstChild).toBe(ref.current);
-    });
-  });
-
-  describe('Elements inspection', () => {
-    it('should be the `p` HTML element by default', () => {
-      render(<Paragraph>Дарья Кловер</Paragraph>);
-
-      const paragraph: HTMLParagraphElement | null = document.querySelector('p');
-      expect(paragraph).toHaveTextContent('Дарья Кловер');
-    });
-
-    it('should be the `i` HTML element', () => {
-      render(<Paragraph component="i">Дарья Кловер</Paragraph>);
-
-      const paragraph: HTMLElement | null = document.querySelector('i');
-      expect(paragraph).toHaveTextContent('Дарья Кловер');
-    });
-
-    it('should have the CSS classes from `p` Typography variant by default', () => {
-      render(<Paragraph>Дарья Кловер</Paragraph>);
-
-      const p: HTMLParagraphElement = screen.getByText('Дарья Кловер');
-      expect(p).toHaveClass(typographyClasses.body1);
-    });
-
-    it('should have the CSS classes from `strong` Typography variant', () => {
-      render(<Paragraph variant="caption">Дарья Кловер</Paragraph>);
-
-      const p: HTMLParagraphElement = screen.getByText('Дарья Кловер');
-      expect(p).toHaveClass(typographyClasses.caption);
-    });
-
-    it('should have gutter', () => {
-      render(<Paragraph>Дарья Кловер</Paragraph>);
-
-      const p: HTMLParagraphElement = screen.getByText('Дарья Кловер');
-      expect(p).toHaveStyle({
-        marginBottom: '0.35em',
-      });
     });
   });
 });
